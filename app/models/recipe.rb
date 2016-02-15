ac2ace8445313b744d8e191663302195672d5caf
Gary Margolis <gsmargolis@outlook.com>
2016-02-15 01:25:27 +0000

    create new and edit forms and partials

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ActiveRecord::Base
-  belongs_to :chefname
+  belongs_to :chef
   validates :chef_id, presence: true
   validates :name, presence: true, length: {minimum: 5, maximum: 100}
   validates :summary, presence: true, length: {minimum: 10, maximum: 150}
