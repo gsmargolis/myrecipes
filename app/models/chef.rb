29a9766c5d35be8e4223c384a1d303a5643c3f11
Gary Margolis <gsmargolis@outlook.com>
2016-03-13 22:33:11 +0000

    Authentication control part 1

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -4,5 +4,6 @@ class Chef < ActiveRecord::Base
   before_save { self.email = email.downcase }
   validates :chefname, presence: true,  length: {minimum: 3, maximum: 39}
   validates :email, presence: true, uniqueness: {case_sensitve: false}, format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
-          message: "not a valid email address"}          
+          message: "not a valid email address"} 
+  has_secure_password
 end
